3cfc30c502b40cd7494c2c395df89425b5e35ce0
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-02-19 17:57:21 -0300

    feat: Implement validators

--- src/http_types/http_request.py
+++ src/http_types/http_request.py
@@ -1,6 +1,5 @@
 # vou separar o que eu vou querer da minha requisição
 class HttpRequest:
-
-    def __int__(self, body: dict, param: dict = None) -> None:
+    def __init__(self, body: dict = None, param: dict=None) -> None:
         self.body = body
         self.status_code = param
--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,12 +1,19 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 # criando um agregador de rotas, rotas relacionadas a eventos estarão aqui
 
 event_route_bp = Blueprint('event_route', __name__) 
 
+from src.validators.events_creator_validator import events_creator_validator
+
 from src.http_types.http_response import HttpResponse
+from src.http_types.http_request import HttpRequest
+
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
+    events_creator_validator(request)
+    http_request = HttpRequest(body = request.json)
+
 
     http_reponse = HttpResponse(body={"estou" : "aqui"}, status_code = 201)
     return jsonify(http_reponse.body), http_reponse.status_code
